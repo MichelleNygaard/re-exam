1eb06febb222c22ea4b861d26284b82866d079d6
Michelle Nygård <mican17@student.sdu.dk>
2025-02-04 17:12:17 +0100

    Fucked around, found out nothing

---
+++ src/main/java/org/example/MachineController.java
@@ -0,0 +1,18 @@
+//package org.example;
+//
+//import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+//
+//public class MachineController {
+//    public static void main(String[] args) {
+//        try {
+//            // getting the Singleton instance and connecting to the server
+//            Connect connection = Connect.getInstance("opc.tcp://192.168.0.122:4840");
+//            OpcUaClient client = connection.getClient();
+//
+//            // methods for controlling the machine would go here
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
