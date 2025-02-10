28293a5e3e753ec9b62acbc89b1987827a336ec3
MichelleNygaard <143798460+MichelleNygaard@users.noreply.github.com>
2025-02-10 12:36:30 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,5 @@ public class Main {
     public static void main(String[] args) throws Exception {
         ConnectionClass.getInstance();
         SpringApplication.run(Main.class, args);
-
-        //start produktionen
-<<<<<<< Updated upstream
-//        Production production = new Production();
-//        production.startProduction(1001, 2, 200, 120);
-//        production.cmdNode();
-=======
-        Production production = new Production();
-        production.startProduction(1001, 2, 200, 120);
-        //production.cmdNode();
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     }
 }
